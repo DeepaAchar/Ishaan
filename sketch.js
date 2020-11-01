1f52f4105e0d7fd25d9f6b045e48d22e1fddd0aa
DeepaAchar <deepav2008@gmail.com>
2020-11-01 18:46:48 +0530

    Add files via upload
    Physics Engine Intro

--- sketch.js
+++ sketch.js
@@ -1,59 +1,45 @@
-var fixedRect, movingRect;
-var rect1, rect2, rect3, rect4;
+const Engine=Matter.Engine;
+const World=Matter.World;
+const Bodies=Matter.Bodies;
+const Body=Matter.Body;
+
+var engine,world;
+
+var object, ground;
 
 function setup() {
   createCanvas(1200,800);
-  fixedRect = createSprite(600, 400, 50, 80);
-  fixedRect.shapeColor = "green";
-  fixedRect.debug = true;
+  engine=Engine.create();
 
-  movingRect = createSprite(400,600,80,30);
-  movingRect.shapeColor = "green";
-  movingRect.debug = true;
+  world=engine.world;
 
-  rect1 = createSprite(200,200,80,30);
-  rect1.shapeColor = "blue";
-  rect1.debug=true;
-  rect1.velocityX=5;
+  var options_ground={
+    isStatic:true,
+    friction:0.2
+  }
 
-  rect2 = createSprite(700,200,80,30);
-  rect2.shapeColor = "purple";
-  rect2.debug=true;
-  rect2.velocityX=-5;
+  var options_box={
+    density: 0.01,
+    restitution: 1,
+    friction:0.05
+  }
 
-  rect3 = createSprite(800,100,80,30);
-  rect3.shapeColor = "yellow";
-  rect3.debug=true;
-  rect3.velocityY=5;
+  object=Bodies.rectangle(600,200,40,40,options_box);
+  World.add(world, object);
+  console.log(object);
 
-  rect4 = createSprite(800,700,80,30);
-  rect4.shapeColor = "orange";
-  rect4.debug=true;
-  rect4.velocityY=-5;
+  ground=Bodies.rectangle(600,790,1200,10,options_ground);
+  World.add(world,ground);
 }
 
 function draw() {
-  background(0,0,0);  
-  movingRect.x = World.mouseX;
-  movingRect.y = World.mouseY;
+  background(255,255,255); 
+  Engine.update(engine); 
+  rectMode(CENTER);
+  fill("green");
+  rect(object.position.x,object.position.y,40,40);
 
-  if (detectCollision(movingRect,fixedRect)) {
-    movingRect.shapeColor = "red";
-    fixedRect.shapeColor = "red";
-  }
-  else {
-    movingRect.shapeColor = "green";
-    fixedRect.shapeColor = "green";
-  }
-  
-  bounceOff(rect1,rect2);
-  
-  bounceOff(rect3,rect4);
-  
-  
-  var total=sum(7,-16);
-  console.log(total);
-
+  fill("brown");
+  rect(ground.position.x,ground.position.y,1200,10);
   drawSprites();
 }
-
